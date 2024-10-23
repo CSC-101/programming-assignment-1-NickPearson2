5912b06e9dc5ca1da9844e06a5e8eb83a33e00ad
Nick Pearson <pearsonnick83@gmail.com>
2024-10-23 16:12:27 -0700

    Completed the assignment

--- hw1.py
+++ hw1.py
@@ -3,26 +3,90 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def vowel_count(string: str) -> int:
+    vowels_list = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for letter in string:
+        if letter.lower() in vowels_list:
+            count += 1
+    return count
 
 # Part 2
+def short_lists(theList: list[list[int]]) -> list[int]:
+    new_list = []
+    for i in theList:
+        if(len(i) == 2):
+            for j in range(len(i)):
+                new_list.append(i[j])
+    return new_list
 
 
 # Part 3
+def ascending_pairs(theList: list[list[int]]) -> list[list[int]]:
+    new_list = []
+    for i in theList:
+        if(len(i) == 2):
+            if(i[0] > i[1]):
+                temp = i[1]
+                i[1] = i[0]
+                i[0] = temp
+                new_list.append(i)
+            else:
+                new_list.append(i)
+        else:
+            new_list.append(i)
 
+    return new_list
 
 # Part 4
-
-
+def add_prices(price1: data.Price, price2: data.Price) -> data.Price:
+    if(price1.cents + price2.cents >= 100):
+        new_price = data.Price(price1.dollars + price2.dollars +1, price1.cents + price2.cents -100)
+        return new_price
+    new_price = data.Price(price1.dollars + price2.dollars, price1.cents + price2.cents)
+    return new_price
 # Part 5
+def rectangle_area(rectangle: data.Rectangle) -> int:
+    length = abs(rectangle.top_left.x - rectangle.bottom_right.x)
+    height = abs(rectangle.top_left.y - rectangle.bottom_right.y)
+    area = length * height
+    return area
 
 
 # Part 6
-
+def books_by_author(name:str, list:list[data.Book]) -> list[data.Book]:
+    book_list = []
+    for book in list:
+        for author in book.authors:
+            if(author == name):
+                book_list.append(book)
+    return book_list
 
 # Part 7
+def circle_bound(rectangle: data.Rectangle) -> data.Circle:
+    #Find center point for circle
+    center_x = abs((rectangle.top_left.x - rectangle.bottom_right.x)/2) + abs(rectangle.top_left.x)
+    center_y = abs((rectangle.top_left.y - rectangle.bottom_right.y)/2) + abs(rectangle.bottom_right.y)
+    center_point = data.Point(center_x, center_y)
+    #Find radius based on which side is longer
+    if(abs((rectangle.top_left.x - rectangle.bottom_right.x)) > abs((rectangle.top_left.y - rectangle.bottom_right.y))):
+        radius = abs((rectangle.top_left.x - rectangle.bottom_right.x)/2)
+    else:
+        radius = abs((rectangle.top_left.y - rectangle.bottom_right.y)/2)
 
+    circle = data.Circle(center_point,radius)
+    return circle
 
 # Part 8
-
-
+def below_pay_average(list:list[data.Employee])->list[str]:
+    total_pay = 0
+    name_list = []
+    if(list == []):
+        return None
+    for employee in list:
+        total_pay += employee.pay_rate
+    avg_pay = total_pay/len(list)
+    for employee in list:
+        if(employee.pay_rate < avg_pay):
+            name_list.append(employee.name)
+    return name_list
--- hw1_test.py
+++ hw1_test.py
@@ -7,29 +7,94 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
-
+    def test_vowel_count1(self):
+        word = "Hello"
+        self.assertEqual(hw1.vowel_count(word), 2)
+    def test_vowel_count2(self):
+        word = "Counted vOwELs"
+        self.assertEqual(hw1.vowel_count(word), 5)
 
     # Part 2
-
-
+    def test_short_lists1(self):
+        list = [[1,2,3],[1,2],[3,4],[1,2,3,4,5],[9,8]]
+        result = [1,2,3,4,9,8]
+        self.assertEqual(hw1.short_lists(list), result)
+    def test_short_lists2(self):
+        list = [[1,2],[1,2],[3,4],[],[9,8]]
+        result = [1,2,1,2,3,4,9,8]
+        self.assertEqual(hw1.short_lists(list), result)
     # Part 3
-
-
+    def test_ascending_Pairs1(self):
+        list = [[1,2,3],[1,2],[3,4],[1,2,3,4,5],[9,8]]
+        result = [[1,2,3],[1,2],[3,4],[1,2,3,4,5],[8,9]]
+        self.assertEqual(hw1.ascending_pairs(list), result)
+    def test_ascending_Pairs2(self):
+        list = [[1,2,3],[1,2],[10,3],[5,2,10,4,5],[9,8]]
+        result = [[1,2,3],[1,2],[3,10],[5,2,10,4,5],[8,9]]
+        self.assertEqual(hw1.ascending_pairs(list), result)
     # Part 4
-
-
+    def test_add_Prices1(self):
+        price1 = data.Price(2, 55)
+        price2=data.Price(4, 55)
+        result = data.Price(7,10)
+        self.assertEqual(hw1.add_Prices(price1, price2), result)
+    def test_add_Prices2(self):
+        price1 = data.Price(2, 10)
+        price2=data.Price(4, 55)
+        result = data.Price(6,65)
+        self.assertEqual(hw1.add_Prices(price1, price2), result)
     # Part 5
-
-
+    def test_rectangle_area1(self):
+        point1 = data.Point(0, 2)
+        point2 = data.Point(10, -1)
+        rectangle = data.Rectangle(point1, point2)
+        self.assertEqual(hw1.rectangle_area(rectangle), 30)
+    def test_rectangle_area2(self):
+        point1 = data.Point(0, 4)
+        point2 = data.Point(8, 0)
+        rectangle = data.Rectangle(point1, point2)
+        self.assertEqual(hw1.rectangle_area(rectangle), 32)
     # Part 6
-
+    def test_books_by_author1(self):
+        book1 = data.Book(['Terry Pratchett','Neil Gaiman' ], 'Good Omens')
+        book2 = data.Book(['Janine Pearson', 'Neil Gaiman'], 'Happy Times')
+        self.assertEqual(hw1.books_by_author('Neil Gaiman',[book1, book2]), [book1, book2])
+    def test_books_by_author2(self):
+        book1 = data.Book(['Terry Pratchett', 'Neil Gaiman'], 'Good Omens')
+        book2 = data.Book(['Janine Pearson', 'Neil Gaiman'], 'Happy Times')
+        book3 = data.Book(["George Orwell","Neil Gaiman" ,"JK Rowling"],'Percy Jackson')
+        book4 = data.Book(["William Golding", "Mark Williams"],'Lord of the Flies')
+        self.assertEqual(hw1.books_by_author('Neil Gaiman', [book1, book2, book3, book4]), [book1, book2, book3])
 
     # Part 7
-
-
+    def test_circle_bound1(self):
+        #Test x bigger than y
+        point1 = data.Point(10, 5)
+        point2 = data.Point(18, 0)
+        center = data.Point(14, 2.5)
+        rectangle = data.Rectangle(point1, point2)
+        result = data.Circle(center, 4)
+        self.assertEqual(hw1.circle_bound(rectangle), result)
+    def test_circle_bound2(self):
+        #Test y bigger than x
+        point1 = data.Point(0, 8)
+        point2 = data.Point(4, 0)
+        center = data.Point(2, 4)
+        rectangle = data.Rectangle(point1, point2)
+        result = data.Circle(center, 4)
+        self.assertEqual(hw1.circle_bound(rectangle), result)
     # Part 8
-
-
+    def test_below_pay_average1(self):
+        person1 = data.Employee("Brennan",20)
+        person2 = data.Employee("Ben",50)
+        person3 = data.Employee("Jack",30)
+        list_empl = [person1, person2, person3]
+        result = ["Brennan", "Jack"]
+        self.assertEqual(hw1.below_pay_average(list_empl), result)
+    def test_below_pay_average_empty(self):
+        list_empl = []
+        result = None
+        self.assertEqual(hw1.below_pay_average(list_empl), result)
 
 
 
